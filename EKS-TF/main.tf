6e9ae9a32451b5ca6d212ff9192b6ef196193645
R5t-web <152695192+R5t-web@users.noreply.github.com>
2024-03-13 15:35:40 +0530

    Update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -11,14 +11,14 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
+resource "aws_iam_role" "cluster-role" {
+  name               = "eks-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.cluster-role.name
 }
 
 #get vpc data
@@ -33,9 +33,9 @@ data "aws_subnets" "public" {
   }
 }
 #cluster provision
-resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
-  role_arn = aws_iam_role.example.arn
+resource "aws_eks_cluster" "cluster" {
+  name     = "eks_cluster"
+  role_arn = aws_iam_role.cluster-role.arn
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
@@ -48,8 +48,8 @@ resource "aws_eks_cluster" "example" {
   ]
 }
 
-resource "aws_iam_role" "example1" {
-  name = "eks-node-group-cloud"
+resource "aws_iam_role" "node-grp-role" {
+  name = "eks-node-group-role"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -65,24 +65,24 @@ resource "aws_iam_role" "example1" {
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example1.name
+  role       = aws_iam_role.node-grp-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example1.name
+  role       = aws_iam_role.node-grp-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example1.name
+  role       = aws_iam_role.node-grp-role.name
 }
 
 #create node group
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "Node-cloud"
-  node_role_arn   = aws_iam_role.example1.arn
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "Node-group"
+  node_role_arn   = aws_iam_role.node-grp-role.arn
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
